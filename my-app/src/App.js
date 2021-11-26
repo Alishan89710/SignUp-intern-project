78523aaf39054e5c862b024914846c1b4a7f9514
Hamza-Mujahid <hamza.mujahid.hussain@gmail.com>
2021-11-26 20:56:03 +0500

    installed ant.design ui

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
 
 function App() {
   return (
     <div className="App">
-     <h1>hello</h1>
+      <h1>hello</h1>
+      <Button type="primary">Primary Button</Button>
     </div>
   );
 }
